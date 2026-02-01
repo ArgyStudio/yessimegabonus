54bf853182e51b6317fb9dcff072e7a45d3ae528
argystudio <argystudio@ejemplo.com>
2026-02-01 20:08:29 +0000

    lines and pixel

---
+++ js/basic-main.js
@@ -0,0 +1,45 @@
+
+document.getElementById("Btn").addEventListener("click", async () => {
+
+
+    document.getElementById("loading-overlay").classList.remove("hidden")
+
+
+
+    const nums = ["5491168655456",
+        "5491168671499",
+        "5491168595444",
+        "5491169707871"
+
+    ]
+
+
+    const randomLine = nums[Math.floor(Math.random() * nums.length)];
+
+
+    if (randomLine) {
+        console.log("ok")
+
+        const msg = `Hola Ganamos! vengo por el bono de bienvenida\nMe creas un usuario?`
+        const url = `https://wa.me/${randomLine}?text=${encodeURIComponent(msg)}`;
+        window.location.href = url
+    }
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load', async () => {
+    document.body.classList.remove("lbody")
+    console.log("load")
+})
